b31dd93e9ca89c9a87c50c720e8ab69b17838bcc
李良奇 <codedance98@gmail.com>
2020-09-10 16:16:50 +0800

    use elementUI with cdn way

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI);
 
 import 'github-markdown-css/github-markdown.css'
 
