db3f34a2cbebc09acf70e61f2cea6b99d410dc8d
李良奇 <codedance98@gmail.com>
2020-09-22 10:55:22 +0800

    update api address

--- src/views/index.vue
+++ src/views/index.vue
@@ -35,7 +35,7 @@ export default {
   methods: {
     getData(){
       this.isLoading = true;
-      fetch('http://8.210.115.7:8081/v1/article/findAll',{
+      fetch('http://api.cd98.xyz/v1/article/findAll',{
         method:'get',
         mode: 'cors',
         headers: {
