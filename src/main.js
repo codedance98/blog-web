015f29724590d03e611cba080171e9010eb3a2f5
李良奇 <codedance98@gmail.com>
2021-06-02 19:46:27 +0800

    Update: 移动端新增长按标题复制url功能

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import "@/assets/iconfont/iconfont.css"
 
 import 'github-markdown-css/github-markdown.css'
 import Highlight from '@/utils/highlight'
+import VueClipboard from 'vue-clipboard2'
+
+Vue.use(VueClipboard)
 Vue.use(Highlight);
 
 Vue.config.productionTip = false
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,9 +2,31 @@
 	<section id='index-page'>
     <div class='container'>
       <Header />
+      <div 
+        class='cover' 
+        @click.stop='coverHandle'
+        v-show='trapIdx !== -1'
+        >
+      </div>
       <div class='list-wrap' v-show='!isLoading'>
         <ul>
-          <li v-for='(item, idx) in data' :key='idx' @click.stop='clickHandle(item)'>{{item.title}}</li>
+          <li 
+            v-for='(item, idx) in data' 
+            :key='idx' 
+            @click.stop='checkClientType(item)'
+            @touchstart.stop='(e)=>{touchstartHandle(e, item, idx)}' 
+            @touchend.stop='(e)=>{touchendHandle(e, item, idx)}'
+            >
+            <span class='title'>
+              {{item.title}}
+              <span 
+                @click.stop='copyURLHandle(item)'
+                v-show='idx === trapIdx' 
+                class='trap'
+                @touchstart.stop
+                >Copy</span>
+            </span>
+            </li>
         </ul>
       </div>
       <div class='loading' v-show='isLoading'>
@@ -16,13 +38,20 @@
 </template>
 
 <script>
+let touchX = ''
+let touchY = ''
 import Header from "@/components/common/header"
 import Footer from "@/components/common/footer"
+import { isMobile } from '@/utils'
 export default {
 	data: () => {
 		return {
       data:[],
-      isLoading: false
+      isLoading: false,
+      touchTimestamp: 0,
+      trapIdx: -1,
+      distanceX: 10,
+      distanceY: 10
 		}
   },
   created(){
@@ -46,12 +75,16 @@ export default {
           return response.json()
         } else {
           return Promise.reject('something went wrong!')
-          alert(`something went wrong!`);
         }
       }).then((response)=>{
         this.isLoading = false;
         this.data = response.data.filter((item)=> {return item.visible === 1});
       })
+    },
+    checkClientType(item) {
+      if(!isMobile()) {
+        this.clickHandle(item)
+      }
     },
     clickHandle(_item){
       let y = new Date(_item.created_at).getFullYear();
@@ -62,14 +95,80 @@ export default {
         path: `/d/${y}/${m}/${d}/${t}`
       })
     },
+    touchstartHandle(e, item, idx) {
+      e.preventDefault()
+      e.stopPropagation()
+      touchX = e.touches[0].pageX
+      touchY = e.touches[0].pageY
+      this.touchTimestamp = Date.now()
+    },
+    touchendHandle(e, item, idx) {
+      let x = e.changedTouches[0].pageX
+      let y = e.changedTouches[0].pageY
+      let actionDuration = Date.now() - this.touchTimestamp > 350
+      if(actionDuration) {
+        this.trapIdx = idx
+      }else if(Math.abs(x - touchX) < this.distanceX && Math.abs(y - touchY) < this.distanceY) {
+        this.clickHandle(item)
+      }
+    },
+    copyURLHandle(_item) {
+      let y = new Date(_item.created_at).getFullYear()
+      let m = new Date(_item.created_at).getMonth()+1
+      let d = new Date(_item.created_at).getDate()
+      let t = _item.title_en
+      this.copyTxt(`${location.href}d/${y}/${m}/${d}/${t}`)
+      this.trapIdx = -1
+    },
+    coverHandle() {
+      this.trapIdx = -1
+    },
     clickGithubHandle(){
       window.open('https://github.com/codedance98')
+    },
+    copyTxt(text){
+      if(typeof document.execCommand!=="function"){
+        return;
+      }
+      var dom = document.createElement("textarea");
+      dom.value = text;
+      dom.setAttribute('style', 'display: block;width: 1px;height: 1px;');
+      document.body.appendChild(dom);
+      dom.select();
+      var result = document.execCommand('copy');
+      document.body.removeChild(dom);
+      if (result) {
+        return;
+      }
+      if(typeof document.createRange!=="function"){
+        return;
+      }
+      var range = document.createRange();
+      var div=document.createElement('div');
+      div.innerhtml=text;
+      div.setAttribute('style', 'height: 1px;fontSize: 1px;overflow: hidden;');
+      document.body.appendChild(div);
+      range.selectNode(div);
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0){
+        selection.removeAllRanges();
+      }
+      selection.addRange(range);
+      document.execCommand('copy');
     }
   }
 }
 </script>
 <style lang="scss">
 #index-page{
+  .cover{
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    z-index: 1;
+    top:0;
+    left:0;
+  }
   .list-wrap{
     color: #1a1a1a;
     box-sizing: border-box;
@@ -80,6 +179,33 @@ export default {
         text-decoration: underline;
         cursor: pointer;
         user-select: none;
+        .title{
+          position: relative;
+          .trap{
+            width: 50px;
+            height: 25px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: rgba(10, 10, 10, .8);
+            border-radius: 4px;
+            position: absolute;
+            z-index: 2;
+            top: -30px;
+            left: 50%;
+            margin-left: -25px;
+            color: rgba(255, 255, 255, .7);
+            &::after{
+              // content: '';
+              // border: 5px solid rgba(10, 10, 10, .8);;
+              // border-bottom-color: transparent;
+              // border-left-color: transparent;
+              // border-right-color: transparent;
+              // position: absolute;
+              // top: 25px;
+            }
+          }
+        }
       }
     }
   }
@@ -88,8 +214,8 @@ export default {
     position:relative;
     .el-icon-loading{
       position: absolute;
-      top:50%;
-      left:50%;
+      top: 50%;
+      left: 50%;
       transform: translate(-50%, -50%);
       font-size: .2rem;
     }
